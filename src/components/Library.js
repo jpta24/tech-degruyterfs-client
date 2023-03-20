a54ece1a227ae9ec33c6f58a2659abd43163ad83
Jpta24 <jeanpierretorres@gmail.com>
2023-03-20 01:05:27 +0100

    pagination done

--- src/components/Library.js
+++ src/components/Library.js
@@ -29,42 +29,141 @@ function Library({ book, setBook, isMobile, setIsMobile, getBookInfo }) {
 			});
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
+	// console.log(listBooks.pagination < 4 ? 1 : listBooks.pagination - 1);
+	const getPagination = (pag) => {
+		axios
+			.get(`${process.env.REACT_APP_SERVER_URL}/api/books/pagination/${pag}`)
+			.then((response) => {
+				setListBooks({
+					...listBooks,
+					list: response.data.list,
+					pagination: pag,
+				});
+			})
+			.catch((error) => {
+				console.log({ error });
+			});
+	};
+	let bookInitial = '';
+	let bookFinal = '';
+	let pagSpot1 = '';
+	let pagSpot2 = '';
+	let pagSpot3 = '';
+	let pagSpot4 = '';
 
-	// const getPagination = (pag) => {
-	// 	axios
-	// 		.get(`${process.env.REACT_APP_SERVER_URL}/api/books/pagination/${pag}`)
-	// 		.then((response) => {
-	// 			setListBooks({
-	// 				...listBooks,
-	// 				list: response.data.list,
-	// 				pagination: pag,
-	// 			});
-	// 		})
-	// 		.catch((error) => {
-	// 			console.log({ error });
-	// 		});
-	// };
+	if (listBooks) {
+		bookInitial = (listBooks.pagination - 1) * 10 + 1;
+		bookFinal =
+			listBooks.pagination === listBooks.totalPag
+				? (listBooks.pagination - 1) * 10 + (listBooks.total % 10)
+				: (listBooks.pagination - 1) * 10 + 10;
+
+		pagSpot1 =
+			listBooks.pagination <= 2
+				? 1
+				: listBooks.pagination >= listBooks.totalPag - 1 ? 
+				listBooks.totalPag - 2
+				: listBooks.pagination - 1;
+		pagSpot2 =
+			listBooks.totalPag > 1 &&
+			(listBooks.pagination <= 2
+				? 2
+				: listBooks.pagination >= listBooks.totalPag - 1
+				? listBooks.totalPag -1
+				: listBooks.pagination);
+		pagSpot3 =
+			listBooks.totalPag > 2 &&
+			(listBooks.pagination <= 2
+				? 3
+				: listBooks.pagination >= listBooks.totalPag - 1
+				? listBooks.totalPag
+				: listBooks.pagination + 1);
+		// pagSpot1 =
+		// 	listBooks.pagination <= 2
+		// 		? 1
+		// 		: listBooks.pagination-1 === listBooks.totalPag - 2
+		// 		? listBooks.totalPag - 2
+		// 		: listBooks.pagination - 1;
+		// pagSpot2 =
+		// 	listBooks.totalPag > 1 &&
+		// 	(listBooks.pagination <= 2
+		// 		? 2
+		// 		: listBooks.pagination === listBooks.totalPag - 1
+		// 		? listBooks.totalPag - 1
+		// 		: listBooks.pagination);
+		// pagSpot3 =
+		// 	listBooks.totalPag > 2 &&
+		// 	(listBooks.pagination <= 2
+		// 		? 3
+		// 		: listBooks.pagination === listBooks.totalPag
+		// 		? listBooks.totalPag
+		// 		: listBooks.pagination + 1);
+		pagSpot4 =
+			pagSpot3 !== listBooks.totalPag ? `...${listBooks.totalPag}` : '';
+	}
 
 	return (
 		<div className={classLibrary}>
-			{listBooks &&
-				listBooks.list.map((elemBook) => {
-					return (
+			<div>
+				{listBooks &&
+					listBooks.list.map((elemBook) => {
+						return (
+							<span
+								className={`isbn-number ${
+									book && elemBook.isbn === book.isbn && 'isbn-active'
+								}`}
+								key={elemBook._id}
+								onClick={() => {
+									setBook(elemBook.isbn);
+									getBookInfo(elemBook._id);
+								}}
+							>
+								<span>{formatIsbn(elemBook.isbn)}</span>
+								<span style={{ marginLeft: '30px' }}>{'>'}</span>
+							</span>
+						);
+					})}
+			</div>
+			{listBooks && (
+				<div className='pagination-container'>
+					<div>
+						<span>{`Showing from ${bookInitial} to ${bookFinal} of ${listBooks.total} books`}</span>
+					</div>
+					<div className='pagination'>
 						<span
-							className={`isbn-number ${
-								book && elemBook.isbn === book.isbn && 'isbn-active'
-							}`}
-							key={elemBook._id}
+							className={
+								pagSpot1 === listBooks.pagination ? 'pagination-active' : ''
+							}
 							onClick={() => {
-								setBook(elemBook.isbn);
-								getBookInfo(elemBook._id);
+								getPagination(pagSpot1);
 							}}
 						>
-							<span>{formatIsbn(elemBook.isbn)}</span>
-							<span style={{ marginLeft: '30px' }}>{'>'}</span>
+							{pagSpot1}
 						</span>
-					);
-				})}
+						<span
+							className={
+								pagSpot2 === listBooks.pagination ? 'pagination-active' : ''
+							}
+							onClick={() => {
+								getPagination(pagSpot2);
+							}}
+						>
+							{pagSpot2}
+						</span>
+						<span
+							className={
+								pagSpot3 === listBooks.pagination ? 'pagination-active' : ''
+							}
+							onClick={() => {
+								getPagination(pagSpot3);
+							}}
+						>
+							{pagSpot3}
+						</span>
+						{listBooks.totalPag > 3 && <span>{pagSpot4}</span>}
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
